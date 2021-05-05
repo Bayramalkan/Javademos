d051cb0fd87dd660275f0998cd23c3ad52400e61
Bayramalkan <61650997+Bayramalkan@users.noreply.github.com>
2021-05-05 03:03:23 +0300

    Delete InstructorManager.java

--- InstructorManager.java
+++
@@ -1,7 +0,0 @@
-package odev3;
-
-public class InstructorManager extends UserManager {
-	public void courseInformation(Instructor instructor) {
-		System.out.println(instructor.getInstructorLesson()+ " kursu " +  instructor.getFirstName() + instructor.getLastName() + " tarafýndan verilmektedir.");
-	}
-}
