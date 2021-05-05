71d0f9b0052c098e00b82469634ed9c63f7df1dc
Bayramalkan <61650997+Bayramalkan@users.noreply.github.com>
2021-05-05 03:03:45 +0300

    Delete StudentManager.java

--- StudentManager.java
+++
@@ -1,8 +0,0 @@
-package odev3;
-
-public class StudentManager extends UserManager{
-	
-	public void getStudent(Student student) {
-		System.out.println(student.getFirstName() +" " +student.getLastName() + " listelendi");
-	}
-}
